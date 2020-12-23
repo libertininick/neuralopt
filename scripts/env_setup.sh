0b6387827de4997d3ca7e8d367144a46851b01a7
libertininick <libertini.nick@gmail.com>
2020-12-23 10:54:14 -0700

    Fix create env script

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-conda update -n base conda
-conda create -y -n neuralopt_env pip
-
+conda create -y -n neuralopt_env python=3.8 pip
 source activate neuralopt_env
-
 python -m pip install -r requirements.txt
-conda install -y -c conda-forge ipykernel ipywidgets==7.5 jupyter jupyter_contrib_nbextensions jupytext nb_conda 
-conda install -y -c anaconda notebook>=5.3 
-conda install -y -c plotly plotly==4.11.0
+python -m ipykernel install --user --name neuralopt_env
