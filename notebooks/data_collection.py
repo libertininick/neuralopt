efc4f1725862c3e87eb6b35493d638a1cd104573
libertininick <libertini.nick@gmail.com>
2020-12-26 13:48:04 -0700

    Add paging to scrape_earnings_date

--- notebooks/data_collection.py
+++ notebooks/data_collection.py
@@ -16,6 +16,7 @@
 # +
 from datetime import datetime
 import glob
+import os
 import random
 import re
 import time
@@ -31,56 +32,86 @@ import requests
 
 # # Scape earnings data
 
-def scrape_earnings_calendar(date):
+def scrape_earnings_date(date):
+    
+    # Create url from date
     url = f'https://finance.yahoo.com/calendar/earnings?day={date}'
-
+    
+    # Send initial request
     response = requests.get(url, timeout=(5,30))
     if response.status_code == 200:
         soup = BeautifulSoup(response.content)
         
-    try:
-        earnings_table = soup.select_one('#cal-res-table')
-        earnings_table = pd.read_html(str(earnings_table))[0]
-        
-        if len(earnings_table):
-            earnings_table.to_csv(f'C:/Users/liber/Dropbox/Investing Models/Earnings data/{date}.csv')
-            return True
-    except:
-        pass
-    
-    return False
+        # Number of events
+        n_events = 0
+        m = soup.select_one('#fin-cal-table > h3 > span.Mstart\(15px\).Fw\(500\).Fz\(s\) > span')
+        if m:
+            m = re.findall(r'(?<=of )[0-9]{1,10}(?= results)', m.text)
+            if m:
+                n_events = int(m[0])
+                
+        if n_events > 0:
+            try:
+                # Extract first 100 results
+                earnings_table = soup.select_one('#cal-res-table')
+                earnings_table = pd.read_html(str(earnings_table))[0]
+                
+                # Page through other results
+                for offset in range(100, n_events, 100):
+                    # Sleep for 2 seconds
+                    time.sleep(2)
+                    
+                    # Add offset to URL
+                    url_i = f'{url}&offset={offset}&size=100'
+                    
+                    # Send ith request
+                    response = requests.get(url_i, timeout=(5,30))
+                    if response.status_code == 200:
+                        soup = BeautifulSoup(response.content)
+                        try:
+                            earnings_table_i = soup.select_one('#cal-res-table')
+                            earnings_table_i = pd.read_html(str(earnings_table_i))[0]
+                            if len(earnings_table_i):
+                                earnings_table = pd.concat([earnings_table, earnings_table_i], axis='rows')
+                        except:
+                            pass
+                        
+                return earnings_table
+                
+            except:
+                return None
 
+
+# ## Scrape every date
 
 # +
 dates = pd.date_range(
-    start='1999-12-31', 
-    end='2010-12-31',
-#     end=datetime.today(), 
+    start='1994-12-31', 
+    end=datetime.today(), 
     freq='D'
 )
-dates = [dt.strftime('%Y-%m-%d') for dt in dates if dt.weekday() < 5]
+dates = [dt.strftime('%Y-%m-%d') for dt in dates]
 print(f'Number of dates: {len(dates):,}')
 
-issues = []
-for dt in dates:
-    result = scrape_earnings_calendar(dt)
-    
-    print(dt, result)
-    if not result:
-        issues.append(dt)
 
-    # Randomly sleep
-    time.sleep(random.randint(0,10))
-
-print()
-print(f'Number of issues: {len(issues):,}')
-print(issues)
+for i, dt in enumerate(dates, 1):
+    result = scrape_earnings_date(dt)
+    if result is not None:
+        # Save
+        result.to_csv(f'../../../Investing Models/Earnings data/{date}.csv')
+        
+        # Randomly sleep
+        time.sleep(random.randint(0,10))
+        
+        # Print progress
+        print(f'{i/len(dates):>6.2%} {date}')
 # -
 
 # # Compile earnings data
 
+# +
 dfs = []
-for f in glob.glob('C:/Users/liber/Dropbox/Investing Models/Earnings data/*.csv'):
+for f in glob.glob('../../../Investing Models/Earnings data/*.csv'):
     m = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', f)
     if m:
         date = m[0]
@@ -93,13 +124,17 @@ for f in glob.glob('C:/Users/liber/Dropbox/Investing Models/Earnings data/*.csv'
         df.index = np.arange(len(df))
         
         dfs.append(df)
+        
+all_earnings_data = pd.concat(dfs, axis='rows')
 
-data = pd.concat(dfs, axis='rows')
-data.to_csv(
-    f'C:/Users/liber/Dropbox/Investing Models/Earnings data/all_data.csv',
+# Save
+all_earnings_data.to_csv(
+    f'../../../Investing Models/Earnings data/all_earnings_data.csv',
     index=False
 )
 
+
+# -
 
 # # Scrape price data
 
@@ -121,7 +156,7 @@ def scrape_price_data(ticker, date_from, date_to):
         if len(price_table):
             price_table['Symbol'] = ticker
             price_table.to_csv(
-                f'C:/Users/liber/Dropbox/Investing Models/Price data/{ticker}.csv',
+                f'../../../Investing Models/Price data/{ticker}.csv',
                 index=False
             )
             return True
@@ -131,23 +166,35 @@ def scrape_price_data(ticker, date_from, date_to):
     return False
 
 
-# +
-tickers = np.unique(data['Symbol'])
-print(f'Number of tickers: {len(tickers):,}')
+# ## Compile tickers from earnings dates
 
-issues = []
-for ticker in tickers:
+# +
+df_earnings = pd.read_csv(f'../../../Investing Models/Earnings data/all_earnings_data.csv')
+tickers = np.unique(df_earnings['Symbol'])
+print(f'All tickers: {len(tickers):,}')
+
+# Data already saved
+tickers_old = [
+    f.replace('../../../Investing Models/Price data\\', '').replace('.csv', '')
+    for f 
+    in glob.glob('../../../Investing Models/Price data/*.csv')
+]
+
+print(f'Old tickers: {len(tickers_old):,}')
+
+# Difference
+tickers_new = set(tickers).difference(tickers_old)
+print(f'New tickers: {len(tickers_new):,}')
+# -
+
+# ## Scrape new tickers
+
+for ticker in tickers_new:
     result = scrape_price_data(ticker, '1995-12-31', '2020-11-30')
-    
-    print(ticker, result)
-    if not result:
-        issues.append(ticker)
 
     # Randomly sleep
     time.sleep(random.randint(0,10))
-
-print()
-print(f'Number of issues: {len(issues):,}')
-# -
+    
+    print(ticker, result)
 
 
