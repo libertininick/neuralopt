a43c20b92c08e6f40c5727f99d45857cec9955c5
libertininick <libertini.nick@gmail.com>
2021-01-30 15:54:59 -0700

    Fix hinterland

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Create a conda environment named `neuralopt_env` 
 conda create -y -n neuralopt_env python=3.7 pip 
 
 # Activate environment 
-source activate neuralopt_env
+source activate neuralopt_env 
 
 # pip install the required Python packages 
 python -m pip install -r requirements.txt 
@@ -20,5 +20,6 @@ conda install -y -c conda-forge jupytext                      # Translates noteb
 jupyter nbextension enable collapsible_headings/main  # Collapsible sections
 jupyter nbextension enable execute_time/ExecuteTime   # Display cell execution time
 jupyter nbextension enable hinterland/hinterland      # Enable code autocompletion
+python -m pip install jedi==0.17.2                    # To make hinterland work with ipython >= 7.18
 jupyter nbextension enable spellchecker/main          # Spellchecking 
 jupyter nbextension enable toc2/main                  # TOC for headers as a sidebar
